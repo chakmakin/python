99886c397937db2ea06805d74848985a7d5a1b5f
CHAKMAK - चकमक <rajeshjhajnv@gmail.com>
2015-10-31 16:29:12 +0530

    Data type conversion in python
    Program for data type conversion in python. Explicit type conversion.

---
+++ datatype.py
@@ -0,0 +1,8 @@
+x = 5
+fx = float(x)   # will convert int x into float fx
+print fx        # will print fx float value
+compx = complex(x)# will convert int x into complex fx
+print compx    # will print fx float value
+str = "5.6"
+strfx = float(str) # will convert string str into float strfx
+print strfx  # will print strfx float value
