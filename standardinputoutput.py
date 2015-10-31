3c942428d0d7f725c0470220ce5098a0e9b702ce
CHAKMAK - चकमक <rajeshjhajnv@gmail.com>
2015-10-31 08:05:52 +0530

    Create standardinputoutput.py
    Example of standard input and output. Will take input from user in raw_input() and give output using print statement on standard output device (monitor).

---
+++ standardinputoutput.py
@@ -0,0 +1,6 @@
+name = raw_input("Enter your  name : ")
+addr = raw_input("Enter address : ")
+amt = input("Enter amount ")
+print "Your  name is  : ",name
+print "Address is : ",addr
+print "Amount entered is : ",amt
